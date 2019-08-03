f12a0645dc7f02c166a880dabda14456d6ed08e5
hfmsolucoes <hfm.solucoes@hotmail.com>
2019-08-03 15:45:51 -0300

    Implementação do useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [techs, setTech] = useState([]);
@@ -20,6 +20,8 @@ function App() {
     localStorage.setItem('tech', JSON.stringify(techs));
   }, [techs]);
 
+  const sizeTechs = useMemo(() => techs.length, [techs]);
+
   return (
     <>
       <ul>
@@ -27,11 +29,16 @@ function App() {
           <li key={tech}>{tech}</li>
         ))}
       </ul>
+      <strong>
+        Você tem {`${sizeTechs} ${sizeTechs === 1 ? 'stack' : 'stacks'}`}
+      </strong>
+      <br />
       <input
         type="text"
         value={newTech}
         onChange={e => setNewTech(e.target.value)}
       />
+
       <button type="button" onClick={handleAdd}>
         Adicionar
       </button>
